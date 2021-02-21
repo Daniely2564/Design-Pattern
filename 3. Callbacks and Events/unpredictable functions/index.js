62f6de6d5b79bb9fa4e769fce2f4defddc976c83
Daniel Yoo <jarkassp2@gmail.com>
2021-02-20 23:26:54 -0500

    Adding Chapter 3

---
+++ 3. Callbacks and Events/unpredictable functions/index.js
@@ -0,0 +1,37 @@
+import { readFile } from "fs";
+
+const cache = new Map();
+
+function inconsistentRead(filename, cb) {
+  if (cache.has(filename)) {
+    // invoked synchronously
+    cb(cache.get(filename));
+  } else {
+    // asynchronous function
+    readFile(filename, "utf8", (err, data) => {
+      cache.set(filename, data);
+      cb(data);
+    });
+  }
+}
+
+function createFileReader(filename) {
+  const listeners = [];
+  inconsistentRead(filename, (value) => {
+    listeners.forEach((listener) => listener(value));
+  });
+
+  return {
+    onDataReady: (listener) => listeners.push(listener),
+  };
+}
+
+const reader1 = createFileReader("../src/data.txt");
+reader1.onDataReady((data) => {
+  console.log(`First call data : ${data}`);
+
+  //   const reader2 = createFileReader("../src/data.txt");
+  //   reader2.onDataReady((data) => {
+  //     console.log(`Second call data : ${data}`);
+  //   });
+});
